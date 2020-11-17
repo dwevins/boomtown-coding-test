09f23386829baff63db13b47f19211b7c99bdf52
Woody Evins <woody.evins@gmail.com>
2020-11-17 18:56:25 -0500

    basic project setup

---
+++ components/body.js
@@ -0,0 +1,7 @@
+const Body = () => (
+  <div id="body">
+    <h2>Body</h2>
+  </div>
+)
+
+export default Body
---
+++ components/footer.js
@@ -0,0 +1,7 @@
+const Footer = () => (
+  <footer>
+    <h3>footer</h3>
+  </footer>
+)
+
+export default Footer
