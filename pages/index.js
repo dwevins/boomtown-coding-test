7049e2374314b7fc929c2974c6a96f2a54cb0d81
Woody Evins <woody.evins@gmail.com>
2020-11-18 16:50:16 -0500

    add link to new note page

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,6 @@ import Link from 'next/link'
 import useSwr from 'swr'
 import { fetcher } from '../utils';
 
-const handleAddNewClick = () => {
-  console.log('adding new note!');
-}
-
 const renderItem = (item, index) => (
   <Link href={ `/notes/${item.id}` } key={index}>
     <a className="flex-grid__item preview">
@@ -31,9 +27,11 @@ const Home = () => {
       <div id="body">
         <div className="content-wrapper">
           <div className="flex-grid">
-            <div className="flex-grid__item preview flex--center" onClick={handleAddNewClick}>
-              <i className="fas fa-plus"></i>
-            </div>
+            <Link href="/new">
+              <a className="flex-grid__item preview flex--center">
+                <i className="fas fa-plus"></i>
+              </a>
+            </Link>
             {data.map((item, index) => renderItem(item, index))}
           </div>
         </div>
