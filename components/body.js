f60867e1bf430a51c7aee633faf45c415ed2362e
Woody Evins <woody.evins@gmail.com>
2020-11-17 20:24:42 -0500

    add list & styles

--- components/body.js
+++ components/body.js
@@ -1,8 +1,17 @@
-const Body = () => (
+const renderItem = item => (
+  <div class="flex-grid__item preview">
+    <h2 className="ta--center">{ item.title }</h2>
+    <p>{ item.content }</p>
+  </div>
+)
+
+const Body = ({
+  notes
+}) => (
   <div id="body">
     <div class="content-wrapper">
       <div class="flex-grid">
-        
+        { notes.map(item => renderItem(item)) }
       </div>
     </div>
   </div>
